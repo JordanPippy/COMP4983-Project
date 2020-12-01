76b28e0769f6e1e4fc55165a706039a372b9b6f6
Jordan <136734p@acadiau.ca>
2020-12-01 18:40:52 -0400

    deploy

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use(express.static('./testprojectweb/build'));
-
+/*
 const con = mysql.createPool({
   host: process.env.JAWSDB_URL,
   user: "e846nrnb17zvxmo8",
@@ -29,7 +29,8 @@ const con = mysql.createPool({
   connectionLimit: 10
 });
 module.exports = con;
-
+*/
+const con = mysql.createConnection(process.env.JAWSDB_URL);
 
 app.get('/CharactersID', function (req, res) {
     // Connecting to the database.

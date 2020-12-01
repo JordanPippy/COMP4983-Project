5641975dbd60709085ab45eda7e3592fb8ee7c8a
Jordan <136734p@acadiau.ca>
2020-12-01 18:43:49 -0400

    deploy

--- testprojectweb/src/App.js
+++ testprojectweb/src/App.js
@@ -5,7 +5,7 @@ import { Button } from 'reactstrap';
 import { Container, Row, Col, Modal, ModalTitle, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 
 //THIS IS THE CONNECTION FOR THE DATABASE. IT NEEDS TO BE CHANGED WHEN USING ON A DIFFERENT COMPUTER
-const dbPath = 'de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306';
+const dbPath = 'mysql://e846nrnb17zvxmo8:mjxnpkmrqx3wudmt@de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/du7bw2qg8ts4oxmj';
 
 /*
  * IconModal: Handles the onClick functionality of all the characters on screen.
